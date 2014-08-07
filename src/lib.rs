50a687daf9b01405a8f26d87475e32ebe939a5a1
Huon Wilson <dbau.pp+github@gmail.com>
2014-08-08 07:40:12 +1000

    Rewrite the macro to be optimised better.
    Code:
    
        cfor!{let (mut i, mut j) = (0u, 0u); i + j < x; { i += 1; j += 1 } {
            test::black_box(i);
        }}
    
    Old IR:
    
        while_cond:                                       ; preds = %while_body, %entry-block
          %i.0 = phi i64 [ 0, %entry-block ], [ %i.1, %while_body ]
          %j.0 = phi i64 [ 0, %entry-block ], [ %j.1, %while_body ]
          %_first.0 = phi i1 [ false, %entry-block ], [ true, %while_body ]
          br i1 %_first.0, label %then-block-107-, label %next-block
    
        while_body:                                       ; preds = %next-block
          call void @llvm.lifetime.start(i64 8, i8* %1) #2
          store i64 %i.1, i64* %dummy.i, align 8
          call void asm "", "r,~{dirflag},~{fpsr},~{flags}"(i64* %dummy.i) #2
          call void @llvm.lifetime.end(i64 8, i8* %1) #2
          br label %while_cond
    
        then-block-107-:                                  ; preds = %while_cond
          %2 = add i64 %i.0, 1
          %3 = add i64 %j.0, 1
          br label %next-block
    
        next-block:                                       ; preds = %while_cond, %then-block-107-
          %i.1 = phi i64 [ %2, %then-block-107- ], [ %i.0, %while_cond ]
          %j.1 = phi i64 [ %3, %then-block-107- ], [ %j.0, %while_cond ]
          %4 = add i64 %j.1, %i.1
          %5 = icmp ult i64 %4, %0
          br i1 %5, label %while_body, label %while_exit
    
    New IR:
    
        next-block:                                       ; preds = %next-block.lr.ph, %next-block
          %j.011 = phi i64 [ 0, %next-block.lr.ph ], [ %3, %next-block ]
          call void @llvm.lifetime.start(i64 8, i8* %2) #2
          store i64 %j.011, i64* %dummy.i, align 8
          call void asm "", "r,~{dirflag},~{fpsr},~{flags}"(i64* %dummy.i) #2
          call void @llvm.lifetime.end(i64 8, i8* %2) #2
          %3 = add i64 %j.011, 1
          %4 = shl i64 %3, 1
          %5 = icmp ult i64 %4, %0
          br i1 %5, label %next-block, label %while_exit.loopexit

--- src/lib.rs
+++ src/lib.rs
@@ -186,17 +186,35 @@ pub macro_rules! cfor {
     // for ($init; $cond; $step) { $body }
     ($init: stmt; $cond: expr; $step: expr $body: block) => {
         {
-            let mut _first = true;
             $init;
-            while {
-                if _first {
-                    _first = false
-                } else {
-                    $step
+            while $cond {
+                let mut _first = true;
+                let mut _continue = false;
+                // this loop runs once, allowing us to use `break` and
+                // `continue` as `goto` to skip forward to the
+                // condition.
+                //
+                // the booleans above are very transparent to the
+                // optimiser, since they are modified exactly once,
+                // with nice control flow, and this this optimises to
+                // be similar to C for loop.
+                loop {
+                    // if we *don't* hit this, there was a `break` in
+                    // the body (otherwise the loop fell-through or
+                    // was `continue`d.)
+                    if !_first { _continue = true; break }
+                    _first = false;
+
+                    $body
+                }
+                if !_continue {
+                    // the `if` wasn't hit, so we should propagate the
+                    // `break`.
+                    break
                 }
 
-                $cond
-            } $body
+                $step
+            }
         }
     };
 }
